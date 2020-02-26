c351a74948b7fcb9beaf8c6620789a890b999d88
root <root@DESKTOP-ORQQVKC.localdomain>
2020-02-26 12:48:04 -0500

    ...

--- views/print_wo.rb
+++ views/print_wo.rb
@@ -78,7 +78,8 @@ row() {
 }.style! flex: 0, 'border-bottom': 'solid 1px black'
 
 self << FlexTable.new() {
-  (o["tasks"] || []).each do |t| 
+  (o["tasks"] || []).each do |t|
+    next unless t 
     t=http(:get, :tasks, t["_id"]) 
     row() {
       code() {span() {t["craft"].ljust(8).gsub(" ", "&nbsp;")}
